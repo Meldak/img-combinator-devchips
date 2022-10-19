1ea40f979eaabf34080ab7d319f5282e4e911dc6
Meldak <meldak.z@gmail.com>
2022-10-18 19:41:32 -0500

    Se establecio orden de ejecucion del objero Combinator

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from modules.Combinator import Combinator
 
 ROOT_DIR = os.path.dirname( os.path.abspath(__file__))
 LAYERS_ROOT = "/mnt/d/Meldak/Proyectos/WIPS/Avatar/Layers/fullsize/"
+OUTPUT_ROOT = "/mnt/d/Meldak/Proyectos/WIPS/Avatar/Combinations"
 
 def main(argv):
     print(ROOT_DIR)    
     os.chdir(ROOT_DIR)
 
-    current = Combinator(LAYERS_ROOT, ROOT_DIR)
-    current.test_read_image()
+    current = Combinator(LAYERS_ROOT, OUTPUT_ROOT,'Background')
+    # current.test_read_image(folder=4)
+    current.set_statics(statics=['Background','Puerco','izq'])
+    current.combinate(True,False)
 
 
 if __name__ == '__main__':
